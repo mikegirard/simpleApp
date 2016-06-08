df4f04f84b102b046d72333af3a82c769e96ae18
Mike Girard <michael.girard@readytalk.com>
2016-06-08 11:31:44 -0600

    Initial project creation from spring starter web

---
+++ src/test/java/org/mgirard/SimpleAppApplicationTests.java
@@ -0,0 +1,18 @@
+package org.mgirard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SimpleAppApplication.class)
+@WebAppConfiguration
+public class SimpleAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
